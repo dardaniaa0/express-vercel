176b1eb75decdf0a66aac1d0ef19a431b396e500
Dardania <70666490+dardaniaa0@users.noreply.github.com>
2021-06-15 17:58:00 -0400

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,36 @@ const { Router } = require('express');
 const { SuccessResponseObject } = require('../common/http');
 const demo = require('./demo.route');
 
+const request = require('request');
+const searchResultParser = require('./search_result_parser');
+
 const r = Router();
 
-r.use('/demo', demo);
+//r.use('/demo', demo);
 
-r.get('/', (req, res) => res.json(new SuccessResponseObject('express vercel boiler plate')));
+//r.get('/', (req, res) => res.json(new SuccessResponseObject('express vercel boiler plate')));
+
+
+r.get('/search', (endpointRequest, endpointResponse) => {
+    let searchTerm = endpointRequest.query.term;
+    
+    request(`https://itunes.apple.com/search?term=${searchTerm}`, { json: true }, (searchError, searchResponse, searchBody) => {
+        if (searchError) {
+            endpointResponse.status(500);
+            endpointResponse.json({errorMessage: 'Failed to search'});
+            return console.log(searchError);
+        }
+
+        const results = searchBody.results;
+        const endpointResponseData = searchResultParser.parse(results);
+
+        endpointResponse.json(endpointResponseData);
+    });
+});
+
+//r.listen(port, () => {
+   // console.log(`Server listening on port: ${port}`);
+//});
+
 
 module.exports = r;
