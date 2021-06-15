43f61bac4727d6f4d84fa0b7a082473074d1d883
Dardania <70666490+dardaniaa0@users.noreply.github.com>
2021-06-15 18:26:38 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,36 @@
 const express = require('express');
 const helmet = require('helmet');
 const { ErrorResponseObject } = require('./common/http');
-const routes = require('./routes');
+//const routes = require('./routes');
 
 const app = express();
+
+const searchResultParser = require('./search_result_parser');
+
+
+app.get('/search', (endpointRequest, endpointResponse) => {
+    let searchTerm = endpointRequest.query.term;
+    
+    request(`https://itunes.apple.com/search?term=${searchTerm}`, { json: true }, (searchError, searchResponse, searchBody) => {
+        if (searchError) {
+            endpointResponse.status(500);
+            endpointResponse.json({errorMessage: 'Failed to search'});
+            return console.log(searchError);
+        }
+
+        const results = searchBody.results;
+        const endpointResponseData = searchResultParser.parse(results);
+
+        endpointResponse.json(endpointResponseData);
+    });
+});
+
+
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(helmet());
-app.use('/', routes);
+//app.use('/', routes);
 
 // default catch all handler
 app.all('*', (req, res) => res.status(404).json(new ErrorResponseObject('route not defined')));
