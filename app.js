26c2d1d909ed5cfedf88d632e9c7f19fefc7deef
Dardania <70666490+dardaniaa0@users.noreply.github.com>
2021-06-15 18:37:34 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const helmet = require('helmet');
+//const helmet = require('helmet');
 //const { ErrorResponseObject } = require('./common/http');
 //const routes = require('./routes');
 
 const app = express();
+const port = 3333;
 
 //const express = require('express');
 const request = require('request');
@@ -26,11 +27,13 @@ app.get('/search', (endpointRequest, endpointResponse) => {
     });
 });
 
+app.listen(port, () => {
+    console.log(`Server listening on port: ${port}`);
+});
 
-
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ extended: true, limit: '50mb' }));
-app.use(helmet());
+//app.use(express.json({ limit: '50mb' }));
+//app.use(express.urlencoded({ extended: true, limit: '50mb' }));
+//app.use(helmet());
 //app.use('/', routes);
 
 // default catch all handler
